d6ae359a07335b14799d19b06adea4ac2fbdd89a
Max <mgoodman5bb@gmail.com>
2021-10-25 19:20:59 -0400

    Basic speech recognition (v1)

--- PROJECTS/voice_recognition/max.py
+++ PROJECTS/voice_recognition/max.py
@@ -1,20 +1,20 @@
+#1.0.0
+
 import speech_recognition as sr
-#SUPER BASIC IDK WHY I COMMIT THIS!!!! -Max G.
 
-r = sr.Recognizer()
-
+# use the audio file as the audio source
 with sr.Microphone() as source:
-    print("Say something!")
+    r = sr.Recognizer()
     audio = r.listen(source)
 
 try:
-    print(f"You said " + audio)
-
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("I heard you say: " + r.recognize_google(audio))
 except sr.UnknownValueError:
-    pass
+    print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
-    print(f"Oops: {e}")
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
-
-#add stuff here.
