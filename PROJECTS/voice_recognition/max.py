102bcdfd6e93cca9b9cc535aff5db3543f66ecbb
Max <75502015+Maxgoodman07@users.noreply.github.com>
2021-10-25 19:21:51 -0400

    Update max.py

--- PROJECTS/voice_recognition/max.py
+++ PROJECTS/voice_recognition/max.py
@@ -8,9 +8,6 @@ with sr.Microphone() as source:
     audio = r.listen(source)
 
 try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
     print("I heard you say: " + r.recognize_google(audio))
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
