0d6e55866d87ca0c630d49dfdc38c1da2bf22847
Zain Ghazanfar <zainlightning@gmail.com>
2021-11-01 15:51:15 -0400

    Test

--- PROJECTS/voice_recognition/zain.py
+++ PROJECTS/voice_recognition/zain.py
@@ -1,3 +1,8 @@
-myName = "Zain"
-print("Hi, my name is " + myName)
+"""
+Maybe add a command where you can say "Search..." 
+and it searches that on Google
 
+Add keyword that activates voice recognition so 
+resources are not wasted continiously running voice 
+recognition software
+"""
