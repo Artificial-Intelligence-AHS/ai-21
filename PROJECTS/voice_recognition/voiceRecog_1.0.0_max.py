8ba2ab15c8874508d5ee782e9136a4b455f93561
Max <mgoodman5bb@gmail.com>
2021-11-01 12:26:12 -0400

    1.0.0 - Added functionality (search) to voicRec...

--- PROJECTS/voice_recognition/voiceRecog_1.0.0_max.py
+++ PROJECTS/voice_recognition/voiceRecog_1.0.0_max.py
@@ -1,17 +1,27 @@
-#1.0.1
+#1.0.0
 
 import speech_recognition as sr
+from googlesearch import search
 
-# use the audio file as the audio source
+
 with sr.Microphone() as source:
     r = sr.Recognizer()
     audio = r.listen(source)
 
 try:
-    print("I heard you say: " + r.recognize_google(audio))
+    sQ = r.recognize_google(audio)
+    print("I'm searching the web for: " + sQ)
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
+qR = search(sQ, tld='com', lang='en', num=3, start=0, stop=3, pause=2)
+
+for i in qR:
+    print(i)
+    print("")
+
 
 
