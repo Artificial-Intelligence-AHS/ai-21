365f0add7ab810e1b515ce321a70687366be15ff
Artifical-Intelligence-AHS <aiclub.ahs@gmail.com>
2021-11-08 12:51:31 -0500

    spectogram and response setuo

---
+++ PROJECTS/voice_recognition/decodingResponse.py
@@ -0,0 +1,37 @@
+import json
+ 
+# Opening JSON file
+fileName = './PROJECTS/voice_recognition/response.json'
+f = open(fileName,)
+ 
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+
+# Printing the first Result
+print(data['results'][0]['alternatives'][0]['transcript'])
+
+# Printing the second Result
+print(data['results'][1]['alternatives'][0]['transcript'])
+
+# Iterating through the json list
+for i in data['results']:
+    print(i['alternative'][0]['transcript'])
+
+# Printing the start time
+print(data['results'][1]['alternatives'][0]['words'][0]['startTime'])
+
+# Converting start time of strings to integer
+print(int((data['results'][1]['alternatives'][0]['words'][0]['startTime']).strip('s')))
+
+
+# Write a program that will return the following response:
+"""
+Word 1: Sphinx with confidence of 80% starting at 0.2 seconds and ending at 0.5 seconds.
+Word 2: Sphinx with confidence of x% starting at 0.2 seconds and ending at 0.5 seconds.
+Word 3: Sphinx with confidence of 80% starting at 0.2 seconds and ending at 0.5 seconds.
+"""
+
+# Closing file
+f.close()
---
+++ PROJECTS/voice_recognition/spectogram.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
+# import the libraries
+local_file_path = '/Users/kunal/Documents/AI-21/ai-21/PROJECTS/voice_recognition/audioFiles/3.wav'
+local_file_path = './PROJECTS/voice_recognition/audioFiles/3.wav'
+
+# Read the wav file (mono)
+samplingFrequency, signalData = wavfile.read(local_file_path)
+print(samplingFrequency)
+
+# Plot the signal read from wav file
+plt.subplot(211)
+plt.title('Spectrogram of a wav file with piano music')
+
+plt.plot(signalData)
+plt.xlabel('Sample')
+plt.ylabel('Amplitude')
+
+plt.subplot(212)
+plt.specgram(signalData,Fs=samplingFrequency)
+plt.xlabel('Time')
+plt.ylabel('Frequency')
+
+plt.show()
+
+
+# OTHER CODE
+"""
+sample_rate, samples = wavfile.read(local_file_path)
+frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
+
+plt.pcolormesh(times, frequencies, spectrogram)
+plt.imshow(spectrogram)
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+plt.show()
+"""
+"""
+import librosa
+import librosa.display
+from pydub import AudioSegment
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from tempfile import mktemp
+local_file_path = '/Users/kunal/Documents/AI-21/ai-21/PROJECTS/voice_recognition/audioFiles/3.mp3'
+
+def plot_mp3_matplot(filename):
+    
+    plot_mp3_matplot -- using matplotlib to simply plot time vs amplitude waveplot
+    
+    Arguments:
+    filename -- filepath to the file that you want to see the waveplot for
+    
+    Returns -- None
+    
+    
+    # sr is for 'sampling rate'
+    # Feel free to adjust it
+    x, sr = librosa.load(filename, sr=44100)
+    plt.figure(figsize=(14, 5))
+    librosa.display.waveplot(x, sr=sr)
+
+def convert_audio_to_spectogram(filename):
+    
+    convert_audio_to_spectogram -- using librosa to simply plot a spectogram
+    
+    Arguments:
+    filename -- filepath to the file that you want to see the waveplot for
+    
+    Returns -- None
+    
+    
+    # sr == sampling rate 
+    x, sr = librosa.load(filename, sr=44100)
+    
+    # stft is short time fourier transform
+    X = librosa.stft(x)
+    
+    # convert the slices to amplitude
+    Xdb = librosa.amplitude_to_db(abs(X))
+    
+    # ... and plot, magic!
+    plt.figure(figsize=(14, 5))
+    librosa.display.specshow(Xdb, sr = sr, x_axis = 'time', y_axis = 'hz')
+    plt.colorbar()
+    
+# same as above, just changed the y_axis from hz to log in the display func    
+def convert_audio_to_spectogram_log(filename):
+    x, sr = librosa.load(filename, sr=44100)
+    X = librosa.stft(x)
+    Xdb = librosa.amplitude_to_db(abs(X))
+    plt.figure(figsize=(14, 5))
+    librosa.display.specshow(Xdb, sr = sr, x_axis = 'time', y_axis = 'log')
+    plt.colorbar()   
+
+plot_mp3_matplot(local_file_path)
+"""
