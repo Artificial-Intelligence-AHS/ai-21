67528f5d9f1afbb5b09a1b0660b05da90a1e4b48
Gtadia <63812286+Gtadia@users.noreply.github.com>
2021-11-15 15:39:59 -0500

    Sup

--- PROJECTS/voice_recognition/min.py
+++ PROJECTS/voice_recognition/min.py
@@ -2,3 +2,28 @@
 1. I want there to be a keyword that activates the voice recognizer
 so that it doesn't respond to you until you say it.
 """
+
+
+# Imports the Google Cloud client library
+from google.cloud import speech  # is equal to from google.cloud.speech  (Is like a file system) 
+
+
+# Instantiates a client
+client = speech.SpeechClient()
+
+# The name of the audio file to transcribe
+gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw" # gs is google's file system (it's like C://)
+
+audio = speech.RecognitionAudio(uri=gcs_uri)
+
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code="en-US",
+)
+
+# Detects speech in the audio filea
+response = client.recognize(config=config, audio=audio)
+
+for result in response.results:
+    print("Transcript: {}".format(result.alternatives[0].transcript))
