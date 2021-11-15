255e6b96688c262ad62198d7488ec6278580fa9d
Max <mgoodman5bb@gmail.com>
2021-11-15 16:30:32 -0500

    max finalize

--- PROJECTS/voice_recognition/StudentFiles/Max.py
+++ PROJECTS/voice_recognition/StudentFiles/Max.py
@@ -1,27 +1,44 @@
-#1.0.0
+"""
+Things to do 
+- Fill out the project section in the github: Link: https://github.com/Artificial-Intelligence-AHS/ai-21/projects/1
+- Create an Archetiure Diagram for this project
+- Edit the readme file to include the project description
+- Create a requirements.txt file to include all the libraries needed for the project
+"""
+"""
+GOOGLE CLOUD PLATFORM PYTHON CODE
+"""
+speech_file = './PROJECTS/voice_recognition/audioFiles/3.wav'
+import json
+from google.oauth2 import service_account
+from google.cloud import speech
+import io
 
-import speech_recognition as sr
-from googlesearch import search
+key_path = './PROJECTS/voice_recognition/keys/voice-recognition-key.json'
+#hello
+with open(key_path, 'r') as j:
+    service_account_info = json.loads(j.read())
+credentials = service_account.Credentials.from_service_account_info(service_account_info)
+client = speech.SpeechClient(credentials=credentials)
 
 
-with sr.Microphone() as source:
-    r = sr.Recognizer()
-    audio = r.listen(source)
+with io.open(speech_file, "rb") as audio_file:
+    content = audio_file.read()
 
-try:
-    sQ = r.recognize_google(audio)
-    print("I'm searching the web for: " + sQ)
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+audio = speech.RecognitionAudio(content=content)
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    language_code="en-US",
+    sample_rate_hertz=8000,
+    #speechContexts = [speech.SpeechContext(phrases=["sphinx"])]
+)
 
+response = client.recognize(config=config, audio=audio)
 
-qR = search(sQ, tld='com', lang='en', num=3, start=0, stop=3, pause=2)
-
-for i in qR:
-    print(i)
-    print("")
-
+# Each result is for a consecutive portion of the audio. Iterate through
+# them to get the transcripts for the entire audio file.
+for result in response.results:
+    # The first alternative is the most likely one for this portion.
+    print(u"Transcript: {}".format(result.alternatives[0].transcript))
 
 
